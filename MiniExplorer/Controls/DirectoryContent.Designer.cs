0a1bc5b2085c37f98764e7bb93b5c83e6a3976e4
LeLapinouAlpha <t-henon@hotmail.fr>
2023-12-31 22:21:12 +0100

    Ajoutez des fichiers projet.

---
+++ MiniExplorer/Controls/DirectoryContent.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MiniExplorer.Controls
+{
+    partial class DirectoryContent
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DirectoryContent));
+            view = new ListView();
+            nameColumn = new ColumnHeader();
+            largeImageList = new ImageList(components);
+            smallImageList = new ImageList(components);
+            SuspendLayout();
+            // 
+            // view
+            // 
+            view.Columns.AddRange(new ColumnHeader[] { nameColumn });
+            view.Dock = DockStyle.Fill;
+            view.FullRowSelect = true;
+            view.LargeImageList = largeImageList;
+            view.Location = new Point(0, 0);
+            view.Name = "view";
+            view.Size = new Size(362, 208);
+            view.SmallImageList = smallImageList;
+            view.TabIndex = 0;
+            view.UseCompatibleStateImageBehavior = false;
+            view.View = View.Details;
+            view.MouseDoubleClick += listView_MouseDoubleClick;
+            // 
+            // nameColumn
+            // 
+            nameColumn.Text = "Name";
+            nameColumn.Width = 256;
+            // 
+            // largeImageList
+            // 
+            largeImageList.ColorDepth = ColorDepth.Depth32Bit;
+            largeImageList.ImageStream = (ImageListStreamer)resources.GetObject("largeImageList.ImageStream");
+            largeImageList.TransparentColor = Color.Transparent;
+            largeImageList.Images.SetKeyName(0, "icons8-dossier-windows-11-color-310.png");
+            largeImageList.Images.SetKeyName(1, "icons8-fichier-windows-11-color-310.png");
+            // 
+            // smallImageList
+            // 
+            smallImageList.ColorDepth = ColorDepth.Depth32Bit;
+            smallImageList.ImageStream = (ImageListStreamer)resources.GetObject("smallImageList.ImageStream");
+            smallImageList.TransparentColor = Color.Transparent;
+            smallImageList.Images.SetKeyName(0, "icons8-dossier-windows-11-color-70.png");
+            smallImageList.Images.SetKeyName(1, "icons8-fichier-windows-11-color-70.png");
+            // 
+            // DirectoryContent
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(view);
+            Name = "DirectoryContent";
+            Size = new Size(362, 208);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView view;
+        private ColumnHeader nameColumn;
+        private ImageList largeImageList;
+        private ImageList smallImageList;
+    }
+}
---
+++ MiniExplorer/Controls/DirectoryContent.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniExplorer.Controls
+{
+    public partial class DirectoryContent : UserControl
+    {
+        /*
+         * **************************************************************************************
+         * *                                 INSTANCE VARIABLES                                 *
+         * **************************************************************************************
+         */
+        private string dirPath;
+        private DirectoryInfo dirInfo;
+
+
+        /*
+         * **************************************************************************************
+         * *                                    CONSTRUCTORS                                    *
+         * **************************************************************************************
+        */
+        public DirectoryContent()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * **************************************************************************************
+         * *                                    PROPERTIES                                      *
+         * **************************************************************************************
+        */
+        public string DirPath
+        {
+            get => dirPath;
+            set
+            {
+                string path = value == null || value.Length == 0 ? Environment.GetLogicalDrives()[0] : value;
+                dirPath = path;
+                dirInfo = new DirectoryInfo(path);
+                Display();
+            }
+        }
+
+        public int ElementCount { get => this.view.Items.Count; }
+        public int SelectedElementCount { get => this.view.SelectedItems.Count; }
+
+        /*
+         * **************************************************************************************
+         * *                                       METHODS                                      *
+         * **************************************************************************************
+        */
+        private void Display()
+        {
+            view.Items.Clear();
+            view.Items.Add(new ListViewItem("..", 0));
+
+            try
+            {
+                foreach (var info in dirInfo.GetDirectories())
+                {
+                    var item = new ListViewItem(info.Name, 0);
+                    view.Items.Add(item);
+                }
+
+                foreach (var info in dirInfo.GetFiles())
+                {
+                    var item = new ListViewItem(info.Name, 1);
+                    view.Items.Add(item);
+                }
+            } 
+            catch (UnauthorizedAccessException uaEx)
+            {
+                MessageBox.Show(uaEx.Message, "Unauthorized access", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (IOException ioEx)
+            {
+                MessageBox.Show(ioEx.Message, "IO exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unhandled exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        /*
+         * **************************************************************************************
+         * *                                EVENTS HANDLER                                      *
+         * **************************************************************************************
+         */
+        private void listView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.view.SelectedItems.Count > 0)
+            {
+                var selectedFile = this.view.SelectedItems[0];
+                var path = Path.Join(DirPath, selectedFile.Text);
+                if (Directory.Exists(path))
+                    DirPath = path;
+                else if (File.Exists(path))
+                    MessageBox.Show("Open file is not handle yet.", "Not implemented", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
